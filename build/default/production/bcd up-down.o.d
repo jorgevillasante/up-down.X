6f9110c9100c2d43a1873314bbd0a9f00b322166
jorgg99 <42448732+jorgg99@users.noreply.github.com>
2018-09-02 22:43:08 -0500

    Initial commit

---
+++ build/default/production/bcd up-down.o.d
@@ -0,0 +1,3 @@
+build/default/production/bcd\ up-down.o:  \
+	C:/Users/ASUS/Desktop/Microcontroladores/MPLABXProjects/up\ down.X/bcd\ up-down.asm  \
+
---
+++ build/default/production/newpic_8b_simple.o.d
@@ -0,0 +1,3 @@
+build/default/production/newpic_8b_simple.o:  \
+	C:/Users/ASUS/Desktop/Microcontroladores/MPLABXProjects/up\ down.X/newpic_8b_simple.asm  \
+
